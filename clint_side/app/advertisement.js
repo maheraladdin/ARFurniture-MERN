581f9c3c3d15071bd8748235d08c1d837d7bc121
maher_aladdin <maheraladdin@std.mans.edu.eg>
2023-04-17 16:42:32 +0200

    UPD advertisement.js & continue_button.js & ADD advertisementsHeader.js & advertisementsViewPager.js

--- clint_side/app/advertisement.js
+++ clint_side/app/advertisement.js
@@ -1,74 +1,25 @@
-import { StyleSheet, View,TouchableOpacity,Image,Dimensions} from "react-native";
-import ArIcon from "../myComponents/adv_page/ar_icon";
-import Slogan from "../myComponents/adv_page/label_light_mode";
+import {StyleSheet, View} from "react-native";
 import ContinueButton from "../myComponents/buttons/continue_button";
-import { useRouter } from "expo-router";
-import PagerView from "react-native-pager-view";
-import { useEffect,useState } from "react";
+import AdvertisementsViewPager from "../myComponents/adv_page/advertisementsViewPager";
+import AdvertisementsHeader from "../myComponents/adv_page/advertisementsHeader";
 
 
 export default function advertisement() {
-	// router to navigate between activities
-	const router = useRouter();
-
-	// images
-	const [images, setImages] = useState([]);
-
-	useEffect(() => {
-		// fetch advertisement from server
-		fetch("http://192.168.1.18:3000/api/advertisements")
-			.then((response) => response.json())
-			.then((data) => {
-				setImages(data);
-			})
-			.catch((error) => console.log("error", error));
-	},[]);
-
 	return (
 		<View style={styles.activity}>
-
 			{/* header */}
-			<View style={styles.container}>
-				<Slogan />
-				<ArIcon />
-			</View>
+			<AdvertisementsHeader />
 
 			{/* viewPager */}
-			<PagerView style={styles.viewPager} initialPage={0}>
-				{
-
-					images.map((image) => {
-						console.log(image.imageURI)
-						return (
-							<View style={styles.page} key={image._id}>
-								<Image
-									source={{uri: image.imageURI}}
-									style={{width: Dimensions.get("window").width, height: "100%"}}
-								/>
-							</View>
-						);
-					})
-				}
-			</PagerView>
+			<AdvertisementsViewPager />
 
 			{/* continue button */}
-			<TouchableOpacity onPress={() => router.push("home")}>
-				<ContinueButton />
-			</TouchableOpacity>
-
+			<ContinueButton activity="home"/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		alignItems: "center",
-		justifyContent: "space-between",
-		flexDirection: "row",
-		alignSelf: "stretch",
-		marginLeft: 10,
-		marginRight: 10
-	},
 	activity: {
 		marginTop: 20,
 		marginBottom: 20,
@@ -76,15 +27,5 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		alignItems: "center",
 		gap: 20,
-	},
-	viewPager: {
-		flex: 1,
-		alignSelf: "stretch",
-		marginLeft: 10,
-		marginRight: 10
-	},
-	page: {
-		justifyContent: 'center',
-		alignItems: 'center',
 	}
 });
---
+++ clint_side/myComponents/adv_page/advertisementsViewPager.js
@@ -0,0 +1,100 @@
+import PagerView from "react-native-pager-view";
+import {Dimensions,StyleSheet, Image, TouchableOpacity, View} from "react-native";
+import {useEffect, useRef, useState} from "react";
+
+
+
+export default function AdvertisementsViewPager() {
+
+    // images
+    const [advertisements, setAdvertisements] = useState([]);
+
+    // page number
+    const [pageNumber, setPageNumber] = useState(0);
+
+    // pager ref
+    const pagerRef = useRef(null);
+
+    // handle page change
+    const handlePageChange = pageNumber => {
+        setPageNumber(pageNumber);
+        pagerRef.current.setPage(pageNumber);
+    };
+
+    useEffect(() => {
+        // fetch advertisement from server
+        fetch("http://192.168.1.18:3000/api/advertisements")
+            .then((response) => response.json())
+            .then((data) => {
+                setAdvertisements(data);
+            })
+            .catch((error) => console.log("error", error));
+    },[]);
+
+    return (
+    <>
+        {/* viewPager */}
+        <PagerView
+            style={styles.viewPager}
+            initialPage={0}
+            ref={pagerRef}
+            onPageScroll={(event) => {
+                setPageNumber(event.nativeEvent.position);
+            }}
+        >
+            {
+
+                advertisements.map((image) => {
+                    console.log(image.imageURI)
+                    return (
+                        <View style={styles.page} key={image._id}>
+                            <Image
+                                source={{uri: image.imageURI}}
+                                style={{width: Dimensions.get("window").width, height: "100%"}}
+                            />
+                        </View>
+                    );
+                })
+            }
+        </PagerView>
+
+        {/* change pages */}
+        <View style={styles.navigation}>
+            {
+                advertisements.map((image, index) => {
+                    return (
+                        <TouchableOpacity key={image._id} onPress={() => handlePageChange(index)}>
+                            <View style={{
+                                width: 30,
+                                height: 30,
+                                borderRadius: "50%",
+                                backgroundColor: index === pageNumber ? "#518379" : "#E0E0E0"
+                            }}>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })
+            }
+        </View>
+    </>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewPager: {
+        flex: 1,
+        alignSelf: "stretch",
+        marginLeft: 10,
+        marginRight: 10
+    },
+    page: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    navigation: {
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 30
+    }
+});
+
